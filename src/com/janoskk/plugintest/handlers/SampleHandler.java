f63a062edd7a73b2fbc6d80a00a61255815189fc
Janos Kasza <bipketto@gmail.com>
2014-09-25 22:24:27 +0200

    Initial commit

---
+++ src/com/janoskk/plugintest/handlers/SampleHandler.java
@@ -0,0 +1,42 @@
+package com.janoskk.plugintest.handlers;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.commands.common.NotDefinedException;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.handlers.HandlerUtil;
+import org.eclipse.jface.dialogs.MessageDialog;
+
+/**
+ * Our sample handler extends AbstractHandler, an IHandler base class.
+ * @see org.eclipse.core.commands.IHandler
+ * @see org.eclipse.core.commands.AbstractHandler
+ */
+public class SampleHandler extends AbstractHandler {
+	/**
+	 * The constructor.
+	 */
+	public SampleHandler() {
+	}
+
+	/**
+	 * the command has been executed, so extract extract the needed information
+	 * from the application context.
+	 */
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
+		
+		String message = "Yo!";
+		try {
+			message = "Yo! You called the \"" + event.getCommand().getName() + "\" command.";
+		} catch (Exception e) {
+		}
+		
+		MessageDialog.openInformation(
+				window.getShell(),
+				"Yo, man!",
+				message);
+		return null;
+	}
+}
